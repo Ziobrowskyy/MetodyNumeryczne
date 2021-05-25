c885be21c0d8b287f26e4b128bfa2cc4bb93491f
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-05-25 17:53:04 +0200

    Utils: Fix and add new vector method to better display polynomials

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -47,6 +47,31 @@ infix operator fun Vector.times(other: Vector): Vector {
     return result
 }
 
+fun vectorToFunctionString(vector: Vector, base: Matrix): String {
+    val sb = StringBuilder(String.format(Locale.ENGLISH, "%.4f * (%s)", vector.first(), vectorToFunctionString(base.first())))
+    for (i in 1 until vector.size) {
+        sb.append(String.format(Locale.ENGLISH, " + %.4f * (%s)", vector[i], vectorToFunctionString(base[i])))
+    }
+    return sb.toString()
+}
+
+fun vectorToFunctionString(vector: Vector): String {
+    val sb = StringBuilder()
+    var first = true
+    for (i in vector.indices) {
+        if (vector[i].equals(0.0, 0.001))
+            continue
+        val xString = when {
+            i == 0 -> ""
+            i == 1 -> "x"
+            else -> "x^$i"
+        }
+        sb.append(String.format(Locale.ENGLISH, "%s%.4f %s", if (first) "" else " + ", vector[i], xString))
+        first = false
+    }
+    return sb.toString()
+}
+
 fun vectorToString(array: DoubleArray): String {
     val sb = StringBuilder("[")
     for (i in 0 until array.size - 1) {
